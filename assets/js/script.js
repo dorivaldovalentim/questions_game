1ca4ca79c86e71d01132706dced6b511212469e4
Dorivaldo Valentim <dorivaldovalentim@gmail.com>
2020-04-22 18:21:19 +0100

    Adicionando alguns estilos css e a primeira interação do usuário com o jogo (responder já está funcional)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,17 @@
 window.onload = function() {
-
+	document.querySelectorAll('.answer-link').forEach(key => {
+		key.addEventListener('click', function() {
+			this.classList.toggle('animated');
+			setTimeout(() => {
+				console.log(this)
+				if(this.getAttribute('data-correct') == 'true') {
+					this.classList.toggle('animated');
+					this.classList.toggle('correct');
+				} else {
+					this.classList.toggle('animated');
+					this.classList.toggle('wrong');
+				}
+			}, 2000);
+		});
+	});
 };
