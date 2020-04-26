46671118e0b4df69a8c8e95bb1db2e75a4657091
Dorivaldo Valentim <dorivaldovalentim@gmail.com>
2020-04-26 16:09:22 +0100

    Adicionando ajax para pegar as perguntas a partir de um json, e os métodos respectivos

--- assets/js/operations.js
+++ assets/js/operations.js
@@ -1,3 +1,85 @@
 const operations = {
+	questions: [],
+	questions_number: 0,
 	
+	question_position: 1,
+	current_question: null,
+
+	getQuestions: function() {
+		var url = '/assets/json/questions.json';
+		var ajax = new XMLHttpRequest();
+		
+		ajax.open('get', url, false);
+		ajax.send();
+
+		if(ajax.status == 200) {
+			this.questions = JSON.parse(ajax.responseText);
+			console.log(this.questions)
+		} else if(ajax.status == 404) {
+			console.log('Não encontrado')
+		}
+	},
+
+	showQuestions: function(question) {
+		this.current_question = question;
+
+		document.querySelector('span.question-position').innerHTML = `Pergunta ${this.question_position} / 10`;
+
+		var structure = `
+			<div class="questions-section">
+				<h1 class="question-text">${this.current_question.text}</h1>
+			</div>
+
+			<div class="answer-section">
+				<ul class="answer-list">
+					<li class="answer-item">
+						<a href="#" class="answer-link" data-correct="${this.current_question.answers[0].status}">
+							<span class="answer-line">A</span>
+							${this.current_question.answers[0].text}
+						</a>
+					</li>
+
+					<li class="answer-item">
+						<a href="#" class="answer-link" data-correct="${this.current_question.answers[1].status}">
+							<span class="answer-line">B</span>
+							${this.current_question.answers[1].text}
+						</a>
+					</li>
+
+					<li class="answer-item">
+						<a href="#" class="answer-link" data-correct="${this.current_question.answers[2].status}">
+							<span class="answer-line">C</span>
+							${this.current_question.answers[2].text}
+						</a>
+					</li>
+
+					<li class="answer-item">
+						<a href="#" class="answer-link" data-correct="${this.current_question.answers[3].status}">
+							<span class="answer-line">D</span>
+							${this.current_question.answers[3].text}
+						</a>
+					</li>
+				</ul>
+			</div>
+		`;
+
+		document.querySelector('section.main-section').innerHTML = structure;
+	},
+
+	changeQuestion: function(question) {
+		console.log(question)
+		this.incrementQuestionPosition();
+		this.current_question = question;
+		this.showQuestions(question);
+	},
+
+	incrementQuestionPosition() {
+		this.question_position++;
+	},
+
+	loadGame: function() {
+		this.getQuestions();
+		this.questions_number = this.questions.length;
+		this.showQuestions(this.questions[0]);
+	}
 };
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,26 @@
 window.onload = function() {
+	operations.loadGame();
+	var i = 1;
+
 	document.querySelectorAll('.answer-link').forEach(key => {
-		key.addEventListener('click', function() {
-			this.classList.toggle('animated');
-			setTimeout(() => {
-				console.log(this)
-				if(this.getAttribute('data-correct') == 'true') {
-					this.classList.toggle('animated');
-					this.classList.toggle('correct');
-				} else {
-					this.classList.toggle('animated');
-					this.classList.toggle('wrong');
-				}
-			}, 2000);
-		});
+		do {
+
+			key.addEventListener('click', function() {
+				this.classList.toggle('animated');
+				setTimeout(() => {
+					if(this.getAttribute('data-correct') == 'true') {
+						i++;
+						this.classList.toggle('animated');
+						this.classList.toggle('correct');
+						operations.changeQuestion(operations.questions[i]);
+					} else {
+						this.classList.toggle('animated');
+						this.classList.toggle('wrong');
+						document.querySelector('[data-correct="true"]').classList.toggle('correct');
+					}
+				}, 2000);
+			});
+			i++;
+		} while(i < operations.questions_number);
 	});
 };
