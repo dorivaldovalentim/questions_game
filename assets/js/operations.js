99cf88e344393234a422cbc847399a73bb55d122
Dorivaldo Valentim <dorivaldovalentim@gmail.com>
2020-05-11 23:13:56 +0100

    Documentando a primeira versão da aplicação

--- assets/js/operations.js
+++ assets/js/operations.js
@@ -1,10 +1,37 @@
+/**************************************************************************
+ * Operations.css
+ *
+ * Folha de script responsável pelas funcionalidades avançadas da aplicação
+ **************************************************************************
+ *
+ * Esta é uma class responsável pelas operações avançadas da aplicação. 
+ */
+
 const operations = {
+	/**
+	 * @class Operations
+	 * 
+	 * @property questions: armazena todas as perguntas do jogo
+	 * @property questions_number: número de perguntas que o jogo contém
+	 * @property question_position: armazena a posição da pergunta actual
+	 * @property current_question: armazena a pergunta actual
+	 */
+
 	questions: [],
 	questions_number: 0,
-	
 	question_position: 1,
 	current_question: null,
 
+	/**
+	 * @method getQuestions()
+	 * 
+	 * Este método acessa um arquivo json por meio de ajax, retorna todo
+	 * o seu conteúdo e armazena-o no atributo questions já convertendo-o
+	 * usando JSON.parse()
+	 * 
+	 * @params any
+	 * @return void
+	 */
 	getQuestions: function() {
 		var url = './assets/json/questions.json';
 		var ajax = new XMLHttpRequest();
@@ -20,6 +47,16 @@ const operations = {
 		}
 	},
 
+	/**
+	 * @method showQuestions()
+	 * 
+	 * Este método recebe um parâmetro (sendo este uma pergunta e suas
+	 * respostas). O que o método faz é somente apresentar a pergunta
+	 * e as respostas no navegador para que o jogador possa ver
+	 * 
+	 * @param {Object} question
+	 * @return void
+	 */
 	showQuestions: function(question) {
 		this.current_question = question;
 
@@ -66,6 +103,18 @@ const operations = {
 		document.querySelector('section.main-section').innerHTML = structure;
 	},
 
+	/**
+	 * @method giveAnswer()
+	 * 
+	 * Como podem ver não sei bem o tipo de parâmetro que passei aqui,
+	 * HTMLSelector é só uma opção.
+	 * Este método recebe um selector (tag) e verifica se um certo atributo
+	 * deste selector tem valor true. Caso tenha retorna true, caso contrário,
+	 * false
+	 * 
+	 * @param {HTMLSelector} answer
+	 * @return boolean
+	 */
 	giveAnswer: function(answer) {
 		return new Promise((resolve, reject) => {
 			setTimeout(() => {
@@ -83,39 +132,93 @@ const operations = {
 		});
 	},
 
+	/**
+	 * @method changeQuestion()
+	 * 
+	 * Este método, assim como o showQuestions() e muitos outros, recebe
+	 * um objecto contendo uma pergunta. E logo depois altera o atributo 
+	 * current_question para a pergunta de parâmetro e chama o método
+	 * showQuestions() para apresentar a nova pergunta na tela.
+	 * 
+	 * @param {Object} question
+	 * @return void
+	 */
 	changeQuestion: function(question) {
 		console.log(question);
 		this.current_question = question;
 		this.showQuestions(question);
 	},
 
+	/**
+	 * @method nextQuestion()
+	 * 
+	 * Este método, não recebe nenhum parâmetro e sua função principal
+	 * é de avançar para a próxima pergunta. Normalmente é chamada quando
+	 * o jogador acerta uma pergunta ou pula usando a ajuda do jogo
+	 * 
+	 * @params any
+	 * @return void
+	 */
 	nextQuestion: function() {
 		this.incrementQuestionPosition();
 
 		if(!(this.question_position > this.questions_number - 1)) {
-			this.current_question = this.questions[this.question_position];
-			this.showQuestions(this.questions[this.question_position]);
+			this.changeQuestion(this.questions[this.question_position]);
 		} else {
 			alert('Está de parabéns por ter terminado o jogo');
 		}
 	},
 
+	/**
+	 * @method incrementQuestionPosition()
+	 * 
+	 * Este método nada mais faz do que incrementar o question_position
+	 * 
+	 * @params any
+	 * @return void
+	 */
 	incrementQuestionPosition: function() {
 		this.question_position++;
 	},
 
+	/**
+	 * @method loadGame()
+	 * 
+	 * Este método nada mais faz do que iniciar o jogo
+	 * 
+	 * @params any
+	 * @return void
+	 */
 	loadGame: function() {
 		this.getQuestions();
 		this.questions_number = this.questions.length;
 		this.showQuestions(this.questions[0]);
 	},
 
+	/**
+	 * @method restart()
+	 * 
+	 * Este método nada mais faz do que reiniciar o jogo quando o 
+	 * jogador responde de forma incorrecta uma pergunta.
+	 * 
+	 * @params any
+	 * @return void
+	 */
 	restart: function() {
 		alert('Errou! Recomeçando')
 		this.question_position = 1;
 		this.showQuestions(this.questions[0]);
 	},
 
+	// AJUDAS DO JOGO
+	/**
+	 * @method skip()
+	 * 
+	 * Este método é uma ajuda do jogo e pula para a pergunta a seguir
+	 * 
+	 * @params any
+	 * @return boolean
+	 */
 	skip: function() {
 		if(!((this.question_position + 1) == this.questions_number)) {
 			this.incrementQuestionPosition();
@@ -127,6 +230,14 @@ const operations = {
 		}
 	},
 
+	/**
+	 * @method idea()
+	 * 
+	 * Este método é uma ajuda do jogo e apresenta a resposta correcta
+	 * 
+	 * @params any
+	 * @return boolean
+	 */
 	idea: function() {
 		var answer = document.querySelector("[data-correct=true]");
 		
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,25 @@
-operations.loadGame();
+/************************************************************************
+ * Script.css
+ *
+ * Folha de script responsável pelas funcionalidades básicas da aplicação
+ ************************************************************************
+ */
 
+
+// Início do jogo
+operations.loadGame();
 getReferences();
 
+/**
+ * @function getReferences()
+ * 
+ * Esta função simplesmente pega as respostas e adiciona
+ * a elas o evento click que redireciona para a função
+ * answer
+ * 
+ * @params any
+ * @return void
+ */
 function getReferences() {
 	document.querySelectorAll('.answer-link').forEach(key => {
 		key.addEventListener('click', answer);
@@ -10,6 +28,19 @@ function getReferences() {
 	console.log(document.querySelectorAll('.answer-link'))
 }
 
+/**
+ * @function answer()
+ * 
+ * Esta função recebe um evento a partir da função getReferences(),
+ * sendo o evento uma resposta e logo depois lança para o método
+ * giveAnswer da class operations que vai verificar se a resposta
+ * está correcta. Caso esteja, a função também é responsável por 
+ * lançar a pergunta a seguir e caso não esteja, manda o jogador
+ * para o início
+ * 
+ * @param {event} event
+ * @return void
+ */
 function answer(event) {
 	var answer = event.target;
 	answer.classList.remove('idea');
@@ -24,6 +55,16 @@ function answer(event) {
 	}).catch(error => console.log(error));
 }
 
+/**
+ * @function skip()
+ * 
+ * Esta função, assim como outras recebe um evento como parâmetro
+ * mas, trata o evento de forma diferente. Esta função está ligada
+ * a uma ajuda da class operations que pula a pergunta actual.
+ * 
+ * @param {event} event
+ * @return void
+ */
 function skip(event) {
 	var element = event.target;
 	var parent = element.parentElement;
@@ -34,7 +75,18 @@ function skip(event) {
 	getReferences();
 }
 
-function idea() {
+/**
+ * @function idea()
+ * 
+ * Esta função, assim como outras recebe um evento como parâmetro
+ * mas, trata o evento de forma diferente. Esta função está ligada
+ * a uma ajuda da class operations que apresenta a resposta correcta
+ * da pergunta actual.
+ * 
+ * @param {event} event
+ * @return void
+ */
+function idea(event) {
 	var element = event.target;
 	var parent = element.parentElement;
 
